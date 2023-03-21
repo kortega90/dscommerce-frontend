ec40ce6e4b235349bbdd56e8217963435ea1af74
KENGI ORTEGA <kortega90@hotmail.com>
2023-03-21 10:45:40 -0300

    Increase/decrease cart items

--- src/routes/ClienteHome/Cart/index.tsx
+++ src/routes/ClienteHome/Cart/index.tsx
@@ -8,7 +8,15 @@ import { Link } from 'react-router-dom';
 export default function Cart() {
 
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
-
+   
+  function handleIncreaseItem(productId: number){
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+  function handleDecreaseItem(productId: number){
+    cartService.decreaseItem(productId);
+    setCart(cartService.getCart());
+  }
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -29,9 +37,9 @@ export default function Cart() {
                         <div className="dsc-cart-item-description">
                           <h3>{item.name}</h3>
                           <div className="dsc-cart-item-quantity-container">
-                            <div className="dsc-cart-item-quantity-btn">-</div>
+                            <div onClick={() => handleDecreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">-</div>
                             <p>{item.quantity}</p>
-                            <div className="dsc-cart-item-quantity-btn">+</div>
+                            <div onClick={() => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
                           </div>
                         </div>
                       </div>
