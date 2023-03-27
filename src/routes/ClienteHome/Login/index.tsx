609281c3b284cbf64ab422bbdc23579e4a146a4d
KENGI ORTEGA <kortega90@hotmail.com>
2023-03-27 13:44:32 -0300

    Login form, login request

--- src/routes/ClienteHome/Login/index.tsx
+++ src/routes/ClienteHome/Login/index.tsx
@@ -1,18 +1,50 @@
 import './styles.css'
+import { useState } from 'react';
+import { CredentialsDTO } from '../../../model/auth';
+import * as authService from "../../../services/auth-service"
 export default function Login(){
+
+  const [formData, setFormData] = useState<CredentialsDTO>({
+    username:'',
+    password:'',
+  })
+  function handleSubmit(event:any){
+    event.preventDefault();
+    authService.loginRequest(formData)
+    .then(response => console.log(response.data) )
+    .catch(error => console.log("error no login", error))
+  }
+  function handleInputChange (event: any){
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({...formData, [name]:value});
+  }
     return(
         <main>
         <section id="login-section" className ="dsc-container">
           <div className ="dsc-login-form-container">
-            <form className ="dsc-card dsc-form">
+            <form className ="dsc-card dsc-form" onSubmit={handleSubmit}>
               <h2>Login</h2>
               <div className ="dsc-form-controls-container">
                 <div>
-                  <input className ="dsc-form-control " type="text" placeholder="Email"/>
+                  <input 
+                  name='username' 
+                  value={formData.username}
+                  className ="dsc-form-control " 
+                  type="text" 
+                  placeholder="Email"
+                  onChange={handleInputChange}/>
+                  
                   <div className ="dsc-form-error"></div>
                 </div>
                 <div>
-                  <input className ="dsc-form-control" type="password" placeholder="Senha"/>
+                  <input 
+                  name='password'
+                  value={formData.password}
+                  className ="dsc-form-control" 
+                  type="password" 
+                  placeholder="Senha"
+                  onChange={handleInputChange}/>
                 </div>
               </div>
   
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,10 +1,22 @@
+import { AxiosRequestConfig } from "axios";
+import QueryString from "qs";
 import { CredentialsDTO } from "../model/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
+import { requestBackend } from "../utils/request";
 
 export function loginRequest(loginData: CredentialsDTO){
-    const header ={
+    const headers ={
         "Content-Type":"application/x-www-form-urlencoded",
-        Authorization: "Basic"+ window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
+        Authorization: "Basic "+ window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
     }
-    
+
+    const requestBody = QueryString.stringify( {...loginData, grant_type: "password"});
+
+    const config: AxiosRequestConfig ={
+        method: "POST",
+        url:"/oauth/token",
+        data: requestBody,
+        headers:headers,
+    }
+   return requestBackend(config);
 }
