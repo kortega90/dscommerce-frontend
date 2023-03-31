7be47df0880b5e0759cacf4e75f3a750267b301c
KENGI ORTEGA <kortega90@hotmail.com>
2023-03-31 13:54:25 -0300

    Select Form 2/2

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,12 +4,17 @@ import { Link, useParams } from "react-router-dom";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms"
 import * as  productService from "../../../services/product-service"
+import * as  categoryService from "../../../services/category-service"
 import FormTextArea from "../../../components/FormTextArea";
+import Select from "react-select";
+import { CategoryDTO } from "../../../model/category";
+import FormSelect from "../../../components/FormSelect";
 
 
 
 export default function ProductForm(){
  const params = useParams();
+ const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
  const isEditing = params.productId !== 'create';
  
@@ -55,7 +60,25 @@ export default function ProductForm(){
       },
       message: "Favor informar uma description minimo de 10 caracteres"
     },
+
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      placeholder: "Categorias",
+      validation: function(value:CategoryDTO[]){
+        return value.length > 0;
+      },
+      message: "Escolha ao menos uma categoria"
+    },
   });
+
+  useEffect(() => {
+    categoryService.findAllRequest()
+    .then( response => {setCategories(response.data)
+    });
+  },[]);
+
   useEffect(() => {
 
     if (isEditing){
@@ -73,6 +96,7 @@ export default function ProductForm(){
   function handleTurnDirty(name: string){
     setFormData(forms.dirtyAndValidate(formData,name));
   }
+
 
     return(
         <main>
@@ -106,6 +130,23 @@ export default function ProductForm(){
                   className="dsc-form-control "
                   onChange={handleInputChange}
                 />
+              </div>
+
+              <div>
+                <FormSelect
+                {...formData.categories}
+                className="dsc-form-control"
+                options={categories} 
+                onChange={(obj:any) => {
+                  const newFormData = forms.update(formData,"categories",obj);
+                  setFormData(newFormData);
+                }}
+                onTurnDirty={handleTurnDirty}
+                isMulti
+                getOptionLabel={(obj:any) => obj.name}
+                getOptionValue={(obj:any) => String (obj.id)}
+                />
+                <div className="dsc-form-error">{formData.categories.message}</div>
               </div>
 
               <div>
---
+++ src/services/category-service.ts
@@ -0,0 +1,10 @@
+import { AxiosRequestConfig } from "axios";
+import { requestBackend } from "../utils/request";
+
+export function findAllRequest() {
+    const config: AxiosRequestConfig = {
+      method: "GET",
+      url: "/categories",
+    };
+    return requestBackend(config);
+  }
